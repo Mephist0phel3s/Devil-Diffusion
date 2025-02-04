6cf9a9a1ca614345bf92557f816b15db1fdd8e2f
Mephist0phel3s <alaskandrifter49@icloud.com>
2025-02-04 00:39:51 +0000

    it is DONE.

--- impl.nix
+++ impl.nix
@@ -29,18 +29,20 @@ pkgs.mkShell rec {
       hardware_deps ++ [
         git # The program instantly crashes if git is not present, even if everything is already downloaded
         python312Full
-	zstd
-	git-lfs
-#	python312Packages.torch
-#	python312Packages.torchWithoutCuda
-#	python312Packages.pip
+        zstd
+        git-lfs
+#	python312Packages.torch               ### python version supersedes, fix later
+#	python312Packages.torchWithoutCuda    ### Apparently torch fucking needs CUDA for some god damn reason despite the test machine being an AMD machine without any CUDA cores.
+                                         #### Repo fucking breaks with this enabled. Fix later.
+#	python312Packages.pip                 ### See reason above.
 #	python312Packages.einops
 #	python312Packages.psutil
-#	python312Packages.safetensors
-#	python312Packages.torchWithRocm
+#	python312Packages.safetensors         ### im not 100% this is borked, but its normally best to enable this alongside torch and pip, buuut they are also fucked so its a moot point. Fix later.
+
+#	python312Packages.torchWithRocm       ### AMD ROCM v6 in the nixpkg repo is marked broken due to an unbuilt dependency, notably HIP. Working on this on the side.
 #	python312Packages.torchaudio
 #	python312Packages.torchvision
-        gperftools
+        gperftools                        #### WARNING::: Needed for memory management, disable at your own risk.
         stdenv.cc.cc.lib
         stdenv.cc
         ncurses5
@@ -54,6 +56,7 @@ pkgs.mkShell rec {
     SOURCE_DATE_EPOCH=$(date +%s)
     export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${LD_LIBRARY_PATH}"
     VENV=venv
+    TMP=tmp
 
     if test ! -d $VENV; then
       python3.12 -m venv $VENV
@@ -61,12 +64,16 @@ pkgs.mkShell rec {
     source ./$VENV/bin/activate
     export PYTHONPATH=`pwd`/$VENV/${pkgs.python312Full.sitePackages}/:$PYTHONPATH
 
+    [ -f devil_scripts/FIRSTRUN.flag ] || { touch devil_scripts/FIRSTRUN.flag && echo -e "First time execution detected. Standby comrade..." && cd devil_scripts && exec ./firstrun.sh; }
+
+    echo "Thank you for using Devil-Diffusion."
+    sleep 4
 
 
-    pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.2
-    pip install -r requirements.txt
-    pip install open-clip-torch
-    python main.py --listen 127.0.0.1
+
+
+    python main.py --listen 127.0.0.1 --auto-launch
+
 
 
 
