365b611d9636589236baceef4c1f75b9269276be
Mephist0phel3s <alaskandrifter49@icloud.com>
2025-02-09 09:58:04 -0600

    Maybe

--- src/devil_scripts/firstrun-AMD.sh
+++ src/devil_scripts/firstrun-AMD.sh
@@ -30,13 +30,13 @@ cd ../data/custom_nodes #### MV to custom node dir and check for existing extens
 
 echo "$PWD"
 
-if [ ! -d ComfyUI-SaveImageWithMetaData ]; then
-    git clone https://github.com/nkchocoai/ComfyUI-SaveImageWithMetaData.git
-  else
-    cd ComfyUI-SaveImageWithMetaData
-    git pull https://github.com/nkchocoai/ComfyUI-SaveImageWithMetaData.git
-    cd ..
-fi
+#if [ ! -d ComfyUI-SaveImageWithMetaData ]; then
+#    git clone https://github.com/nkchocoai/ComfyUI-SaveImageWithMetaData.git
+#  else
+#    cd ComfyUI-SaveImageWithMetaData
+#    git pull https://github.com/nkchocoai/ComfyUI-SaveImageWithMetaData.git
+#    cd ..
+#fi
 
 
 if [ ! -d comfyui-manager ]; then
@@ -46,11 +46,11 @@ if [ ! -d comfyui-manager ]; then
 fi
 
 
-if [ ! -d ComfyUI-ImageMetadataExtension ];then
-    git clone https://github.com/edelvarden/ComfyUI-ImageMetadataExtension.git
-  else cd ComfyUI-ImageMetadataExtension
-    git pull https://github.com/edelvarden/ComfyUI-ImageMetadataExtension.git && cd ..
-fi
+#if [ ! -d ComfyUI-ImageMetadataExtension ];then
+#    git clone https://github.com/edelvarden/ComfyUI-ImageMetadataExtension.git
+#  else cd ComfyUI-ImageMetadataExtension
+#    git pull https://github.com/edelvarden/ComfyUI-ImageMetadataExtension.git && cd ..
+#fi
 
 
 if [ ! -d ComfyUI-Crystools ]; then
@@ -66,29 +66,27 @@ if [ ! -d ComfyUI-Easy-Use ]; then
     cd ComfyUI-Easy-Use
     pip install -r requirements.txt
   else
-    cd ComfyUI-Easy-Use && git pull && cd ..
+    cd ComfyUI-Easy-Use && git pull && pip install -r requirements.txt && cd ..
 fi
 
 
-if [ ! -d ComfyUI-YCYY-LoraInfo ]; then
-    git clone https://github.com/ycyy/ComfyUI-YCYY-LoraInfo.git
-  else
-    cd ComfyUI-YCYY-LoraInfo && git pull && cd ..
-fi
+#if [ ! -d ComfyUI-YCYY-LoraInfo ]; then
+#    git clone https://github.com/ycyy/ComfyUI-YCYY-LoraInfo.git
+#  else
+#    cd ComfyUI-YCYY-LoraInfo && git pull && cd ..
+#fi
 
 
 #### Move back to src dir
-cd ../../src
 #echo "$PWD"
 #exit 1
-
-mkdir -p tmp
-cd tmp
+cd ../..
 
 echo "Cloning Devil-Diffusion base model + VAE, CLIP vision."
 echo -e "NOTE::: Devilv1.3 base model comes with VAE baked in.n/VAE on the side is a clone of the baked in VAE for ease of access for certain nodes, some nodes really REALLY want a specified VAE for some reason ive yet to figure out."
-sleep 5
-
+sleep 1
+mkdir -p tmp
+cd tmp
 if [ ! -d Devil-Diffusion ]; then
     git-lfs clone https://huggingface.co/Mephist0phel3s/Devil-Diffusion
   else
@@ -100,30 +98,27 @@ if [ ! -d IP-Adapter ]; then
   else
     cd IP-Adapter && git pull && cd ..
 fi
-
+cd ..
 echo "$PWD"
-exit 1
+
+
 
 #echo "Creating model directories for Stable Diffusion core so python and the default SD models that dont really work dont get patched in over Devil."
 
-mkdir -p ../../data/docs/ipadapter
-mkdir -p ../../data/models/ipadapter
+mkdir -p docs/ipadapter
+mkdir -p data/models/ipadapter
 
-cp Devil-Diffusion/CLIP-ViT-H-14-laion2B-s32B-b79K.safetensors ../../data/models/clip_vision
-cp Devil-Diffusion/CLIP-ViT-bigG-14-laion2B-39B-b160k.safetensors ../../data/models/clip_vision
-cp Devil-Diffusion/Devil_Pony_v1.3.safetensors ../../data/models/checkpoints
-cp Devil-Diffusion/Devil_VAE.safetensors ../../data/models/vae
-cp -r IP-Adapter/models/* ../../data/models/ipadapter
-cp -r IP-Adapter/sdxl_models/* ../../data/models/ipadapter
-cp IP-Adapter/README.md ../../data/docs/ipadapter/README.md
-cd ..
-
-
-
+cp tmp/Devil-Diffusion/CLIP-ViT-H-14-laion2B-s32B-b79K.safetensors models/clip_vision
+cp tmp/Devil-Diffusion/CLIP-ViT-bigG-14-laion2B-39B-b160k.safetensors models/clip_vision
+cp tmp/Devil-Diffusion/Devil_Pony_v1.3.safetensors models/checkpoints
+cp tmp/Devil-Diffusion/Devil_VAE.safetensors models/vae
+cp -r tmp/IP-Adapter/models/* models/ipadapter
+cp -r tmp/IP-Adapter/sdxl_models/* models/ipadapter
+cp tmp/IP-Adapter/README.md docs/ipadapter/README.md
 echo "$PWD"
-exit 1
-
+cd ..
 exec bash -c "./devil-AMD.sh"
+exit 1
 
 
 
