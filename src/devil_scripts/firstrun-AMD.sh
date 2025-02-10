599bf77e72b7280b2da507b4b31f55238098da28
Mephist0phel3s <alaskandrifter49@icloud.com>
2025-02-09 22:09:58 -0600

    123

--- src/devil_scripts/firstrun-AMD.sh
+++ src/devil_scripts/firstrun-AMD.sh
@@ -6,52 +6,30 @@ echo "The reason i decided to implement auto install model functionality is beca
 
 echo "Executing first run script"
 
-#echo "DEBUG::: where am i? ==" && $PWD
-cd ..
 mkdir -p models ../data/
 mkdir -p input ../data/
 mkdir -p output ../data/
 mkdir -p temp ../data/
 mkdir -p custom_nodes ../data/
 
-
 cp -r models ../data/
 cp -r input ../data/
 cp -r output ../data/
 cp -r temp ../data/
 cp -r custom_nodes ../data/
+
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.2
 pip install -r requirements.txt
 pip install open-clip-torch
 
 cd ../data/custom_nodes #### MV to custom node dir and check for existing extensions.
-
-
-
 echo "$PWD"
-
-#if [ ! -d ComfyUI-SaveImageWithMetaData ]; then
-#    git clone https://github.com/nkchocoai/ComfyUI-SaveImageWithMetaData.git
-#  else
-#    cd ComfyUI-SaveImageWithMetaData
-#    git pull https://github.com/nkchocoai/ComfyUI-SaveImageWithMetaData.git
-#    cd ..
-#fi
-
 
 if [ ! -d comfyui-manager ]; then
     git clone  https://github.com/ltdrdata/ComfyUI-Manager
   else
     git pull https://github.com/ltdrdata/ComfyUI-Manager
 fi
-
-
-#if [ ! -d ComfyUI-ImageMetadataExtension ];then
-#    git clone https://github.com/edelvarden/ComfyUI-ImageMetadataExtension.git
-#  else cd ComfyUI-ImageMetadataExtension
-#    git pull https://github.com/edelvarden/ComfyUI-ImageMetadataExtension.git && cd ..
-#fi
-
 
 if [ ! -d ComfyUI-Crystools ]; then
     git clone -b AMD https://github.com/crystian/ComfyUI-Crystools.git
@@ -68,18 +46,6 @@ if [ ! -d ComfyUI-Easy-Use ]; then
   else
     cd ComfyUI-Easy-Use && git pull && pip install -r requirements.txt && cd ..
 fi
-
-
-#if [ ! -d ComfyUI-YCYY-LoraInfo ]; then
-#    git clone https://github.com/ycyy/ComfyUI-YCYY-LoraInfo.git
-#  else
-#    cd ComfyUI-YCYY-LoraInfo && git pull && cd ..
-#fi
-
-
-#### Move back to src dir
-#echo "$PWD"
-#exit 1
 cd ../..
 
 echo "Cloning Devil-Diffusion base model + VAE, CLIP vision."
@@ -99,11 +65,6 @@ if [ ! -d IP-Adapter ]; then
     cd IP-Adapter && git pull && cd ..
 fi
 cd ..
-echo "$PWD"
-
-
-
-#echo "Creating model directories for Stable Diffusion core so python and the default SD models that dont really work dont get patched in over Devil."
 
 mkdir -p docs/ipadapter
 mkdir -p data/models/ipadapter
--- src/shell.nix
+++ src/shell.nix
@@ -2,6 +2,25 @@
 
 let
   pkgs = import <nixpkgs> { };
+  console.colors = [
+    "073642"
+    "dc322f"
+    "859900"
+    "b58900"
+    "268bd2"
+    "d33682"
+    "2aa198"
+    "eee8d5"
+    "002b36"
+    "cb4b16"
+    "586e75"
+    "657b83"
+    "839496"
+    "6c71c4"
+    "93a1a1"
+    "fdf6e3"
+  ];
+
 
   # Define hardware dependencies based on the variant
   hardware_deps = with pkgs;
@@ -21,6 +40,9 @@ let
       gcc
     ] else if variant == "ROCM" then [
       rocmPackages.rocm-runtime
+      rocmPackages.rocm-smi
+#      rocmPackages.hipblas
+#      rocmPackages.roctracer
       pciutils
     ] else if variant == "CPU" then [
     ] else throw "You need to specify which variant you want: CPU, ROCm, or CUDA.";
@@ -34,8 +56,6 @@ in pkgs.mkShell rec {
     hardware_deps ++ [
       git # The program instantly crashes if git is not present, even if everything is already downloaded
       python312Full
-      ffmpeg_6-full
-      rsync
       zstd
       git-lfs
       # python312Packages.torch               ### python version supersedes, fix later
@@ -49,7 +69,7 @@ in pkgs.mkShell rec {
       # python312Packages.torchWithRocm       ### AMD ROCM v6 in the nixpkg repo is marked broken due to an unbuilt dependency, notably HIP. Working on this on the side.
       # python312Packages.torchaudio
       # python312Packages.torchvision
-      gperftools                        #### WARNING::: Needed for memory management, disable at your own risk.
+      gperftools                              #### WARNING::: Needed for memory management, disable at your own risk.
       stdenv.cc.cc.lib
       stdenv.cc
       ncurses5
@@ -62,11 +82,15 @@ in pkgs.mkShell rec {
 
   # Custom shell hook for setting up environment
   shellHook = ''
+
     SOURCE_DATE_EPOCH=$(date +%s)
     export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${LD_LIBRARY_PATH}"
     VENV=venv
     TMP=tmp
     export VARIANT="${variant}"
+    export RED='\033[0;31m'
+    export NC='\033[0m'
+    export GREEN='\033[0;32m'
 
 
     # Create virtual environment if it doesn't exist
@@ -75,52 +99,161 @@ in pkgs.mkShell rec {
     fi
     source ./$VENV/bin/activate
     export PYTHONPATH=`pwd`/$VENV/${pkgs.python312Full.sitePackages}/:$PYTHONPATH
+    cd ..
+    GitRoot="$PWD"
+
+        if [ ! -f devil_scripts/FIRSTRUN.flag ]; then
+            touch devil_scripts/FIRSTRUN.flag
+            echo -e "First time execution detected. Standby comrade..."
+            mkdir -p models $GitRoot/data/
+            mkdir -p input $GitRoot/data/
+            mkdir -p output $GitRoot/data/
+            mkdir -p temp $GitRoot/data/
+            mkdir -p custom_nodes $GitRoot/data/
+
+            cp -r $GitRoot/src/models $GitRoot/data/
+            cp -r $GitRoot/src/input $GitRoot/data/
+            cp -r $GitRoot/src/output $GitRoot/data/
+            cp -r $GitRoot/src/temp $GitRoot/data/
+            cp -r $GitRoot/src/custom_nodes $GitRoot/data/
+            cd $GitRoot
+        fi
 
 
 
-    if [ ! -f devil_scripts/FIRSTRUN.flag ]; then
-      touch devil_scripts/FIRSTRUN.flag
-      echo -e "First time execution detected. Standby comrade..."
-      mkdir -p ../data/input
-      mkdir -p ../data/temp
-      mkdir -p ../data/user
-      mkdir -p ../data/custom_nodes
-      mkdir -p ../data/models
-            # Select the appropriate firstrun script based on the variant
-      case "$VARIANT" in
-        "ROCM")
-          echo "Running first run script for AMD/ROCm..."
-          cd devil_scripts && exec ./firstrun-AMD.sh
+############# Custom nodes
+
+        if [ ! -d $GitRoot/data/custom_nodes/ComfyUI-Manager ]; then
+            git clone  https://github.com/ltdrdata/ComfyUI-Manager $GitRoot/data/custom_nodes/ComfyUI-Manager
+        else
+           cd $GitRoot/data/custom_nodes/ComfyUI-Manager
+           git pull https://github.com/ltdrdata/ComfyUI-Manager
+           cd $GitRoot
+        fi
+
+        if [ ! -d $GitRoot/data/custom_nodes/ComfyUI-Crystools ]; then
+            git clone -b AMD https://github.com/crystian/ComfyUI-Crystools.git $GitRoot/data/custom_nodes/ComfyUI-Crystools
+            cd $GitRoot/data/custom_nodes/ComfyUI-Crystools
+            pip install -r requirements.txt
+            cd $GitRoot
+          else
+            cd $GitRoot/data/custom_nodes/ComfyUI-Crystools
+            git pull https://github.com/crystian/ComfyUI-Crystools.git
+            cd $GitRoot
+        fi
+
+
+        if [ ! -d $GitRoot/data/custom_nodes/ComfyUI-Easy-Use ]; then
+            git clone https://github.com/yolain/ComfyUI-Easy-Use $GitRoot/data/custom_nodes/ComfyUI-Easy-Use
+            cd $GitRoot/data/custom_nodes/ComfyUI-Easy-Use
+            pip install -r requirements.txt
+            cd $GitRoot
+         else
+            cd $GitRoot/data/custom_nodes/ComfyUI-Easy-Use
+            git pull
+            pip install -r requirements.txt
+            cd $GitRoot
+        fi
+
+############################## Cloning the models
+
+    echo "Cloning Devil-Diffusion base model + VAE, CLIP vision."
+    echo -e "NOTE::: Devilv1.3 base model comes with VAE baked in.n/VAE on the side is a clone of the baked in VAE for ease of access for certain nodes, some nodes really REALLY want a specified VAE for some reason ive yet to figure out."
+
+    sleep 1
+    tmp="$GitRoot/data/tmp"
+    mkdir -p tmp
+        if [ ! -d $tmp/Devil-Diffusion ]; then
+        echo -e "Depending on your network speed, this may be a good time to go to the bathroom or grab coffee. \nFirst execution takes a bit to pull and build initially."
+        echo -e "NOTICE: This start script will not run again unless you wipe this entire directory. \nThis script can be bypassed in future installs by first running n/touch devil-scripts/FIRSTRUN.flag before running the nix-shell."
+            git-lfs clone https://huggingface.co/Mephist0phel3s/Devil-Diffusion $tmp/Devil-Diffusion
+              else
+            git-lfs pull https://huggingface.co/Mephist0phel3s/Devil-Diffusion $tmp/Devil-Diffusion
+        fi
+
+        if [ ! -d $tmp/IP-Adapter ]; then
+            git-lfs clone https://huggingface.co/h94/IP-Adapter $tmp/IP-Adapter
+        else
+            git-lfs pull https://huggingface.co/h94/IP-Adapter $tmp/IP-Adapter
+            ipa=$tmp/IP-Adapter && export !!
+        fi
+
+
+# Create directories
+mkdir -p "$GitRoot/docs/ipadapter" "$ipa"
+
+# Rsync for copying files and directories
+rsync -av --progress \
+    "$tmp/Devil-Diffusion/CLIP-ViT-H-14-laion2B-s32B-b79K.safetensors" "$GitRoot/data/models/clip_vision" \
+    "$tmp/Devil-Diffusion/CLIP-ViT-bigG-14-laion2B-39B-b160k.safetensors" "$GitRoot/data/models/clip_vision" \
+    "$tmp/Devil-Diffusion/Devil_Pony_v1.3.safetensors" "$GitRoot/data/models/checkpoints" \
+    "$tmp/Devil-Diffusion/Devil_VAE.safetensors" "$GitRoot/data/models/vae" \
+    "$tmp/IP-Adapter/README.md" "$GitRoot/docs/ipadapter/README.md"
+
+# Rsync for copying directories recursively
+rsync -av --progress \
+    "$tmp/IP-Adapter/models/*" "$ipa" \
+    "$tmp/IP-Adapter/sdxl_models/*" "$ipar"
+
+# Return to source directory
+cd "$GitRoot/src"
+
+
+
+
+    case "$VARIANT" in
+      "ROCM")
+        cd $GitRoot/src/
+
+        echo "Running first run script for AMD/ROCm..."
+
+
+        echo "The reason i decided to implement auto install model functionality is because they were a pain in the ass to find, i suffered so you dont have to"
+        echo "Executing first run script"
+
+
+        pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.2.4
+        pip install -r requirements.txt
+        pip install open-clip-torch
+
+       echo "$GREENThank you for using $REDDevil-Diffusion."
+        TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1 PYTORCH_TUNABLEOP_ENABLED=1 python main.py --listen 127.0.0.1 --auto-launch --port 8666 --base-directory $GitRoot/data \
+                           --use-pytorch-cross-attention --disable-cuda-malloc
           ;;
-        "CUDA")
-          echo "Running first run script for NVIDIA/CUDA..."
-          cd devil_scripts && exec ./firstrun-NVIDIA.sh
+      "CUDA")
+        echo "Running first run script for NVIDIA/CUDA..."
+        echo -e "Depending on your network speed, this may be a good time to go to the bathroom or grab coffee. First execution takes a bit to pull and build initially."
+        echo -e "NOTICE::: This start script will not run again unless you wipe this entire directory. \nThis script can be bypassed in future installs by first running touch devil-scripts/FIRSTRUN.flag before running the nix-shell."
+        echo "The reason i decided to implement auto install model functionality is because they were a pain in the ass to find, i suffered so you dont have to"
+        echo "Executing first run script"
+        pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu126
+        pip install -r requirements.txt
+        pip install open-clip-torch
+        bash -c 'printf "Thank you for using Devil-Diffusion."'
+        NIXPKGS_ALLOW_UNFREE=1 python main.py --listen 127.0.0.1 --auto-launch --port 8666 --base-directory $GitRoot/data \
+                       --cuda-malloc
           ;;
-        "CPU")
-          echo "Running first run script for CPU..."
-          cd devil_scripts && exec ./firstrunCPU.sh
+      "CPU")
+        echo "Running first run script for CPU..."
           ;;
         *)
-          echo "Unknown variant: $VARIANT. Please specify a valid variant."
-          exit 1
+        echo "Unknown variant: $VARIANT. Please specify a valid variant."
+        exit 1
           ;;
-      esac
-    fi
-    RED='\033[0;31m'
-    NC='\033[0m'
-    printf 'Thank you for using $REDDevil-Diffusion.'
-    sleep 1
+    esac
 
-    python main.py --listen 127.0.0.1 --auto-launch --port 8666 --base-directory ../data
+
+
   '';
 
   # Post shell hook to set Python environment properly
   postShellHook = ''
-    ln -sf ${pkgs.python312Full.sitePackages}/* ./.venv/lib/python3.12/site-packages
+    ln -sf ${pkgs.python312Full.sitePackages}/* ./venv/lib/python3.12/site-packages
   '';
 
   # Environment variables
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
   CUDA_PATH = pkgs.lib.optionalString (variant == "CUDA") pkgs.cudatoolkit;
+  ROCM_PATH= pkgs.lib.optionalString ( variant == "ROCM") pkgs.rocmPackages.rocm-smi;
   EXTRA_LDFLAGS = pkgs.lib.optionalString (variant == "CUDA") "-L${pkgs.linuxPackages.nvidia_x11}/lib";
 }
